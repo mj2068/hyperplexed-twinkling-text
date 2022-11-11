4c4ba5cfec2339e41787ec5d8c60872d0ca17d99
mj2068 <yuelianghushenfu@sina.com>
2022-11-11 10:37:07 +0800

    semi-solved the initial state 2nd after elements

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 let index = 0,
-  interval = 3000;
+  interval = 1000;
 
 const rand = (min, max) => {
-  let r = Math.floor(Math.random() * (max - min + 1)) + min;
-  // console.log("random number: ", r);
-  return r;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 let stars = document.getElementsByClassName("magic-star");
@@ -12,19 +10,19 @@ let stars = document.getElementsByClassName("magic-star");
 const animate = (star) => {
   star.style.setProperty("--star-left", `${rand(-10, 100)}%`);
   star.style.setProperty("--star-top", `${rand(-40, 100)}%`);
+  // without the following line will result in some elements not being processed
+  // by this function, and they will show their static state
+  star.style.display = "block";
+
   star.style.animation = "none";
+  // DOM reflow
   star.offsetHeight;
-  star.style.animation = "";
+  star.style.animation = "scale 700ms ease forwards";
 };
 
 for (const star of stars) {
-  // console.log(star);
-  // let a = index++ * (interval / stars.length);
-  // console.log("a", stars.length, a);
-
   setTimeout(() => {
-    console.log(Date.now());
     animate(star);
-    // setInterval(() => animate(star), 1000);
-  }, index++ * 1000);
+    setInterval(() => animate(star), 1000);
+  }, index++ * (interval / stars.length));
 }
